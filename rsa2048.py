c9d1d7f4c384d0b020d371b76e9c0a6e457e65b4
Karl Zander <karl.zander.medicated@gmail.com>
2024-03-14 15:06:11 -0700

    rsa2048 faster

--- rsa2048.py
+++ rsa2048.py
@@ -7,6 +7,7 @@ import time
 # Estimated 838 days to factor
 
 n = 25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784406918290641249515082189298559149176184502808489120072844992687392807287776735971418347270261896375014971824691165077613379859095700097330459748808428401797429100642458691817195118746121515172654632282216869987549182422433637259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133844143603833904414952634432190114657544454178424020924616515723350778707749817125772467962926386356373289912154831438167899885040445364023527381951378636564391212010397122822120720357
+print(len(str(n)))
 
 def isqrt(n):
   l = int(gmpy2.log(n))
@@ -30,7 +31,7 @@ def fermat(n, verbose=True):
     q = n // a
     return p, q
 
-def ztx(n, pk, ctxt):
+def ztx(n, psize):
     sq = isqrt(n)
     sq2 = sq * 2
     sq3 = sq * 3
@@ -49,7 +50,18 @@ def ztx(n, pk, ctxt):
     x = (n2 - sq2) * 2
     #a = ((n2 - sq) * 1) + sqs
     y = ((n2 - sq) * 2) + (sqs * 2)
-    a = sq + (sqs * l * ll) + sql
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + sqs)
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * l) + ((ll ** ll * (psize * psize))))
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * l) - (ll ** ll * l * ll * ll * l))
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * l) - (ll ** ll * l * ll * ll * l))
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * l) - (ll ** ll * l * ll * ll * ll))
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * l) + (ll ** ll * l)) + sql
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * l) + (ll ** ll * l)) + (sql + (ll ** ll))
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * l) + (ll ** ll * l)) - (sql + (ll ** ll) + sql)
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * l) + (ll ** ll * l)) - (sql + (ll ** ll) + sql)
+    #a = (sq + (sqs * l * ll) + sql) + (((l * l * l * l) * ll) + (sqs * ll) + (ll ** ll * l))
+    #a = sq + (sqs * ll * l * ll)
+    a = sq + (psize * psize)
     if a % 2 == 0:
         a += 1
     #a = ((n2 - sq) * 2)
@@ -66,7 +78,7 @@ def ztx(n, pk, ctxt):
     #while ((n % a) != x):
     #while b*b != b2:
         #a = ((a - 2) % y)
-        a += 2
+        a -= 2
         #b2 = a*a - n
         #b = isqrt(b2)
         #try:
@@ -75,8 +87,8 @@ def ztx(n, pk, ctxt):
         #    continue
         #ptxt = decrypt(ctxt, Osk, n)
         #print(ptxt)
-        steps += 1
-    print("ztx steps", steps)
+        #steps += 1
+    #print("ztx steps", steps)
     print("result", a, a % sq)
     #p=a+b
     #q=a-b
@@ -103,7 +115,7 @@ psize = 32
 pk = 0
 ctxt = 0
 s = time.time()
-ap, aq = ztx(n, pk, ctxt)
+ap, aq = ztx(n, psize)
 e = time.time()
 print(e - s)
 print(ap, aq)
